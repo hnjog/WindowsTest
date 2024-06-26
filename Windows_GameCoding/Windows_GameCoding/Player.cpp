a116ed90b0ad4a4d2320fd5770e4ab54682b499a
Hyun <hnjogo@gmail.com>
2024-06-26 15:45:52 +0900

    Update Player.cpp

--- Windows_GameCoding/Windows_GameCoding/Player.cpp
+++ Windows_GameCoding/Windows_GameCoding/Player.cpp
@@ -1,5 +1,6 @@
 #include "pch.h"
 #include"Player.h"
+#include"InputManager.h"
 
 Player::Player ( )
 {
@@ -17,6 +18,11 @@ void Player::BeginPlay ( )
 void Player::Tick ( )
 {
 	Super::Tick ( );
+
+	if ( GET_SINGLE ( InputManager )->GetButton ( KeyType::W ) )
+	{
+
+	}
 }
 
 void Player::Render ( HDC hdc )
