f8417efff55c54a740538c809b8833de7a29dd69
Hyun <hnjogo@gmail.com>
2024-07-18 12:44:57 +0900

    Test UIs

--- Windows_GameCoding/Windows_GameCoding/DevScene.cpp
+++ Windows_GameCoding/Windows_GameCoding/DevScene.cpp
@@ -14,6 +14,7 @@
 #include"CollisionManager.h"
 #include"UI.h"
 #include"Button.h"
+#include"TestPanel.h"
 
 DevScene::DevScene ( )
 {
@@ -121,13 +122,7 @@ void DevScene::Init ( )
 	}
 
 	{
-		Button* ui = new Button ( );
-
-		ui->SetSprite ( GET_SINGLE ( ResourceManager )->GetSprite ( L"Start_Off" ),BS_Default );
-		ui->SetSprite ( GET_SINGLE ( ResourceManager )->GetSprite ( L"Start_On" ),BS_Clicked );
-
-		ui->SetPos ( { 200,200 } );
-
+		TestPanel* ui = new TestPanel ( );
 		_uis.push_back ( ui );
 	}
 
